ddef4797c5263eda43bb3208217cd268f2e7b497
hoffmann <koehn.sebastian@googlemail.com>
2018-02-20 09:07:44 +0100

    better url

--- lib/Steemit.pm
+++ lib/Steemit.pm
@@ -5,7 +5,7 @@ use Mojo::Base -base;
 use Mojo::UserAgent;
 use Mojo::JSON qw(decode_json encode_json);
 
-has url     => 'https://rpc.steemliberator.com';
+has url     => 'https://steemd.steemitstage.com';
 has ua      =>  sub { Mojo::UserAgent->new };
 
 
