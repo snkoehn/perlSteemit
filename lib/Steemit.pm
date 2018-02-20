8b012676eb96cdb14693c02ef018bba601fefd10
hoffmann <koehn.sebastian@googlemail.com>
2018-02-20 09:09:02 +0100

    better server

--- lib/Steemit.pm
+++ lib/Steemit.pm
@@ -5,7 +5,7 @@ use Mojo::Base -base;
 use Mojo::UserAgent;
 use Mojo::JSON qw(decode_json encode_json);
 
-has url     => 'steemd.minnowsupportproject.org';
+has url     => 'https://steemd.steemitstage.com';
 has ua      =>  sub { Mojo::UserAgent->new };
 
 
