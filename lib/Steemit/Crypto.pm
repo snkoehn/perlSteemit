84d2d758c89da4482b361f8632c19e73c14ebf34
hoffmann <koehn.sebastian@googlemail.com>
2018-02-24 18:59:40 +0100

    add method for public key generation

--- lib/Steemit/Crypto.pm
+++ lib/Steemit/Crypto.pm
@@ -7,4 +7,13 @@ use Carp;
 my $curve = Math::EllipticCurve::Prime->from_name('secp256k1');
 
 
+sub get_public_key_point {
+   my( $key ) = @_;
+
+   die "key needs to be a Math::BigInt Object " unless( $key->isa('Math::BigInt') );
+
+   my $public_key = $curve->g->multiply( $key );
+
+   return $public_key;
+}
 1;
