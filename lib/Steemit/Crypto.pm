63528d08aa0e281836d15d5af99dc49ce09a8cc2
hoffmann <koehn.sebastian@googlemail.com>
2018-02-24 19:01:32 +0100

    add methods to recover y coordinate and respective point when only x and a recovery factor i is given wich specifies whether the y coordinate in Np is even or odd

--- lib/Steemit/Crypto.pm
+++ lib/Steemit/Crypto.pm
@@ -7,6 +7,29 @@ use Carp;
 my $curve = Math::EllipticCurve::Prime->from_name('secp256k1');
 
 
+sub point_from_x {
+   my ( $x,$i ) = @_;
+   my $y = recover_y( $x, $i );
+   return Math::EllipticCurve::Prime::Point->new(
+      x => $x,
+      y => $y,
+      curve => $curve
+   );
+}
+
+sub recover_y {
+   my ( $x,$i ) = @_;
+   my $curve = Math::EllipticCurve::Prime->from_name('secp256k1');
+   my ($p, $a, $b) = ($curve->p, $curve->a, $curve->b);
+   $x = $x->copy;
+
+   my $y = ($x->bmodpow(3,$p)+$a*$x+$b)->bmodpow(($p+1)/4,$p);
+
+   $y = $p - $y if $i%2 ne $y%2;
+   return $y;
+}
+
+
 sub get_public_key_point {
    my( $key ) = @_;
 
