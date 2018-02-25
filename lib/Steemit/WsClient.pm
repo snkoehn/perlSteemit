561413277b0b40fa1a4b471a0e5cbe3c7772c94e
hoffmann <koehn.sebastian@googlemail.com>
2018-02-25 13:57:06 +0100

    bump up version

--- lib/Steemit/WsClient.pm
+++ lib/Steemit/WsClient.pm
@@ -6,11 +6,11 @@ Steemit::WsClient - perl lirary for interacting with the steemit websocket servi
 
 =head1 VERSION
 
-Version 0.05
+Version 0.06
 
 =cut
 
-our $VERSION = '0.05';
+our $VERSION = '0.06';
 
 
 =head1 SYNOPSIS
