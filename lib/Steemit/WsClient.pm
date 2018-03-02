7db72d7b3e04d7fd39aaa4ead714b0843e8de9c1
hoffmann <koehn.sebastian@googlemail.com>
2018-03-02 05:25:09 +0100

    dynamic lookup of operation id

--- lib/Steemit/WsClient.pm
+++ lib/Steemit/WsClient.pm
@@ -435,7 +435,8 @@ sub _serialize_transaction_message  {
       my ($operation_name,$operations_parameters) = @$operation;
 
       ##operation id
-      $serialized_transaction .= pack "C", 0;
+      my $operation_id = $self->_index_of_operation($operation_name);
+      $serialized_transaction .= pack "C", $operation_id;
 
       $serialized_transaction .= pack "C", length $operations_parameters->{voter};
       $serialized_transaction .= pack "A*", $operations_parameters->{voter};
@@ -454,6 +455,77 @@ sub _serialize_transaction_message  {
 
    return pack( 'H*', ( '0' x 64 )).$serialized_transaction;
 }
+
+sub _index_of_operation {
+   my ( $self, $operation ) = @_;
+
+   my @operations = qw(
+   vote
+   comment
+   transfer
+   transfer_to_vesting
+   withdraw_vesting
+   limit_order_create
+   limit_order_cancel
+   feed_publish
+   convert
+   account_create
+   account_update
+   witness_update
+   account_witness_vote
+   account_witness_proxy
+   pow
+   custom
+   report_over_production
+   delete_comment
+   custom_json
+   comment_options
+   set_withdraw_vesting_route
+   limit_order_create2
+   challenge_authority
+   prove_authority
+   request_account_recovery
+   recover_account
+   change_recovery_account
+   escrow_transfer
+   escrow_dispute
+   escrow_release
+   pow2
+   escrow_approve
+   transfer_to_savings
+   transfer_from_savings
+   cancel_transfer_from_savings
+   custom_binary
+   decline_voting_rights
+   reset_account
+   set_reset_account
+   claim_reward_balance
+   delegate_vesting_shares
+   account_create_with_delegation
+   fill_convert_request
+   author_reward
+   curation_reward
+   comment_reward
+   liquidity_reward
+   interest
+   fill_vesting_withdraw
+   fill_order
+   shutdown_witness
+   fill_transfer_from_savings
+   hardfork
+   comment_payout_update
+   return_vesting_delegation
+   comment_benefactor_reward
+   );
+   unless( $self->{_op_index} ){
+      my $count = 0;
+      $self->{_op_index} = { map { $_ => $count++ } @operations };
+   }
+   return $self->{_op_index}{$operation} // die "$operation not defined";
+
+}
+
+
 
 
 
